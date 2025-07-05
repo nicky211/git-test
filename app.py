236b336a907693e259e3e70510f5f84bf1b1a3b0
nicky211 <andreevnikita21@gmail.com>
2025-07-05 17:25:45 +0500

    Добавил Streamlit-приложение

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
-print("Привет, GitHub! Это мой первый тестовый проект.")
+import streamlit as st
+import pandas as pd
+
+st.title("Моё первое Streamlit-приложение")
+st.write("Привет! Это тестовый проект.")
+
+if st.button("Показать данные"):
+    df = pd.DataFrame({
+        "Число": [1, 2, 3, 4],
+        "Квадрат": [1, 4, 9, 16]
+    })
+    st.write(df)
+    st.line_chart(df.set_index("Число"))
